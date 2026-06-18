403f8627f83e66e36e7825e9f01b23c64256e848
Fincito200 <aaronrodrigo2004@hotmail.com>
2026-06-18 15:09:18 -0500

    FIXEADO PARA PONER BD

--- src/main/java/com/utp/ProyectoIntegrador/controller/LoginController.java
+++ src/main/java/com/utp/ProyectoIntegrador/controller/LoginController.java
@@ -1,18 +1,13 @@
 package com.utp.ProyectoIntegrador.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login")
-    public String login(@RequestParam(required=false) String error,
-                        @RequestParam(required=false) String logout, Model m) {
-        if (error  != null) m.addAttribute("error",  "Credenciales incorrectas.");
-        if (logout != null) m.addAttribute("logout", "Sesión cerrada correctamente.");
-        return "login";
-    }
+
     @GetMapping("/")
-    public String root() { return "redirect:/login"; }
+    public String root() {
+        return "redirect:/index.html";
+    }
 }
